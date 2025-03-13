422645efca352e4260ba4b062d4e0acbf8394b41
Mendes <162505010+M3ndezs@users.noreply.github.com>
2025-03-12 23:14:40 -0300

    Add files via upload
    Atividade 10

---
+++ Atividade9/src/atividade9/Atividade10/src/atividade10/Atividade10.java
@@ -0,0 +1,16 @@
+
+package atividade10;
+
+/**
+ *
+ * @author MENDES
+ * DATA: 12/03/2025
+ */
+public class Atividade10 {
+
+    public static void main(String[] args) {
+        Funcionario func = new Funcionario("Xiru", 44, 2550.00);
+        func.exibirInformacoes();
+    }
+    
+}
---
+++ Atividade9/src/atividade9/Atividade10/src/atividade10/Funcionario.java
@@ -0,0 +1,25 @@
+
+package atividade10;
+
+public class Funcionario extends Pessoa{
+  private double salario;
+
+    public Funcionario(String nome, int idade, double salario) {
+        super(nome, idade);
+        this.salario = salario;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+  
+  @Override
+    public void exibirInformacoes(){
+        super.exibirInformacoes();
+        System.out.println("Salário: " + salario);
+    }
+}
